1d7cd2eb7757ce9205cae6894d4fb587bb562c57
mikril <mikril10426@gmail.com>
2023-10-03 19:09:08 +0500

    add CardList

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,37 @@
 import Main from '../../pages/main/main';
 import Card from '../card/card';
-type ImgProps={
-    imgPath:string,
-    imgName:string
-  }
-function App({imgPath,imgName}: ImgProps): JSX.Element{
+
+const imgComps= [
+  {imgPath:"img/fantastic-beasts-the-crimes-of-grindelwald.jpg",imgName:"Fantastic Beasts: The Crimes of Grindelwald"},
+  {imgPath:"img/bohemian-rhapsody.jpg",imgName:"Bohemian Rhapsody"},
+  {imgPath:"img/macbeth.jpg",imgName:"Macbeth"},
+  {imgPath:"img/aviator.jpg",imgName:"Aviator"},
+  {imgPath:"img/we-need-to-talk-about-kevin.jpg",imgName:"We need to talk about Kevin"},
+  {imgPath:"img/what-we-do-in-the-shadows.jpg",imgName:"What We Do in the Shadows"},
+  {imgPath:"img/revenant.jpg",imgName:"Revenant"},
+  {imgPath:"img/johnny-english.jpg",imgName:"Johnny English"},
+  {imgPath:"img/shutter-island.jpg",imgName:"Shutter Island"},
+  {imgPath:"img/pulp-fiction.jpg",imgName:"Pulp Fiction"},
+  {imgPath:"img/no-country-for-old-men.jpg",imgName:"No Country for Old Men"},
+  {imgPath:"img/snatch.jpg",imgName:"Snatch"},
+  {imgPath:"img/moonrise-kingdom.jpg",imgName:"Moonrise Kingdom"},
+  {imgPath:"img/seven-years-in-tibet.jpg",imgName:"Seven Years in Tibet"},
+  {imgPath:"img/midnight-special.jpg",imgName:"Midnight Special"},
+  {imgPath:"img/war-of-the-worlds.jpg",imgName:"War of the Worlds"},
+  {imgPath:"img/dardjeeling-limited.jpg",imgName:"Dardjeeling Limited"},
+  {imgPath:"img/orlando.jpg",imgName:"Orlando"},
+  {imgPath:"img/mindhunter.jpg",imgName:"Mindhunter"},
+  {imgPath:"img/midnight-special.jpg",imgName:"Midnight Special"},
+]
+
+const Cards :JSX.Element[] = imgComps.map((element) =>
+<Card imgPath={element.imgPath} imgName={element.imgName}/>
+);
+
+function App(): JSX.Element{
+   
     return(
-     <Card imgPath={imgPath} imgName={imgName} />
+      < Main Cards={Cards}  />
     );
 }
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-
-
-const ImgComp=[{"img/fantastic-beasts-the-crimes-of-grindelwald.jpg":"Fantastic Beasts: The Crimes of Grindelwald"},{"img/bohemian-rhapsody.jpg","Bohemian Rhapsody"}]
-  
-
-
 root.render(
   <React.StrictMode>
-    <>
-      <App></App>
-    </>
+    <App/>
   </React.StrictMode>
 );
