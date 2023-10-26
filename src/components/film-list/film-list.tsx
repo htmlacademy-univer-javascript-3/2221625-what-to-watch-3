fc4b2e912a713e66e308eff48ff239562e0c71c6
mikril <mikril10426@gmail.com>
2023-10-26 12:09:34 +0500

    change

--- src/components/film-list/film-list.tsx
+++ src/components/film-list/film-list.tsx
@@ -15,31 +15,31 @@ type FilmComp = {
   director:string;
   starring:string;
 }
-function FilmList({ filmComps, genre }: { filmComps: Array<FilmComp>; genre: string | undefined}):JSX.Element{
+function FilmList({ filmComps, genre }: { filmComps: Array<FilmComp>, genre: string | undefined}):JSX.Element{
   return(
-    <div>
+    <>
       {genre !== undefined ? (
-        filmComps
-          .filter((element) => element.genre === genre)
-          .slice(0, 4)
-          .map((element) => (
-            <Card
-              key={`Film ${element.id}`}
-              id={element.id}
-              imgPath={element.cardImgPath}
-              imgName={element.name}
-              videoPath={element.videoPath}
-            />
-          ))
-      ) : (
+      filmComps
+        .filter((element) => element.genre === genre)
+        .slice(0, 4)
+        .map((element) => (
+          <Card
+            key={`Film ${element.id}`}
+            id={element.id}
+            imgPath={element.cardImgPath}
+            imgName={element.name}
+            videoPath={element.videoPath}
+          />
+        ))
+    ) : (
         filmComps.map((element) => {
           if (element.id !== '0') {
             return <Card key={`Film ${element.id}`} id={element.id} imgPath={element.cardImgPath} imgName={element.name} videoPath={element.videoPath} />;
           }
-
+          
         })
       )}
-    </div>
+    </>
   );
 }
 export default FilmList;
